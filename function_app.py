507173d974644a426b30948f93884ec5be1d72fe
LKrew005 <lkrew005@gmail.com>
2024-03-24 20:02:21 -0500

    updating logging and gitignore

--- function_app.py
+++ function_app.py
@@ -7,7 +7,6 @@ app = func.FunctionApp()
 @app.schedule(schedule="0 0 12 * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def MissouriLegislationTracker(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
+
     twitbot.main()
-    logging.info('Python timer trigger function executed.')
+    logging.info('Run Completed')
