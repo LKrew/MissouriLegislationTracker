c9f252d566791c7264b68b51de41fcbabe9d2bef
LKrew005 <lkrew005@gmail.com>
2024-03-25 21:04:10 -0500

    Adding dynamic session id retrieval

--- shared/twitbot.py
+++ shared/twitbot.py
@@ -13,9 +13,13 @@ def main():
     target_strings = ["third read", "passed", "passes", "perfect", "introduced" ]
     api_key = os.environ['LEGISCAN_API_KEY']
     api_url = f"https://api.legiscan.com/?key={api_key}&op="
-    bill_id = ""
+    get_session_list_uri = "getSessionList&state=MO"
+    sessions = requests.get(api_url + get_session_list_uri).json()
+    session_id = sessions['sessions'][0]['session_id']
+    bill_id = ''
     get_bill_uri = f"getBill&id="
-    bill_list = requests.get(api_url + 'getMasterList&id=2122').json()['masterlist']
+    get_bill_list_uri = f'getMasterList&id={session_id}'
+    bill_list = requests.get(api_url + get_bill_list_uri).json()['masterlist']
     bills = []
     for b in reversed(bill_list.items()):
         if b[0] == 'session': continue
@@ -31,29 +35,31 @@ def main():
                         bill.sponsors = [f"{person['name']} of District {person['district']} ({person['party']})" for person in bill_details['sponsors']]
                         bills.append(bill)
     bills = sorted(bills, key=lambda x: x.last_action_date)
-    print(len(bills))
     logging.info(f'Todays Bills: {len(bills)}')
     twitter_client = get_twitter_client()
     mast_client = get_mastodon_client()
     bsky_client = get_client()
+    twitter_post_count = 0
+    mast_post_count = 0
+    bsky_post_count = 0
     for bill in bills:
         try:
-            logging.info("Posting to Twitter")
+            twitter_post_count += 1
             send_tweet(bill, twitter_client)
         except:
             logging.exception("Failed to Post to Twitter/X")
         try: 
-            logging.info("Posting to Bsky")
+            bsky_post_count += 1
             post_to_bsky(bill, bsky_client)
         except:
             logging.exception("Failed to Post to Blue Sky")
         try:
-            logging.info("Posting to Mastodon")
+            mast_post_count += 1
             send_post_to_mastodon(bill, mast_client)
         except:
             logging.exception("Failed to Post to Mastodon")
-    logging.info("Completed Run")
-## Commented out for Azure Funtion Deployment, Uncomment to run via Bash Script
+    logging.info(f"Completed Run: \nTwitter Posts: {twitter_post_count} \nBsky Posts: {bsky_post_count}\nMastodon Posts: {mast_post_count}")
+
 if __name__ == "__main__":
     load_dotenv('.env')
     print(datetime.strftime(date.today(), '%Y-%m-%d'))
